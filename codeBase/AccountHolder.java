5c326bc42a42eb35e095e1dec60d92ff61fb4982
Arnab Mahato <arnabmahato1010@gmail.com>
2023-03-26 20:37:41 +0530

    first commit : ATM Interface

---
+++ codeBase/AccountHolder.java
@@ -0,0 +1,26 @@
+class AccountHolder
+{
+   
+   private String id = "SBI103";
+   private String pin = "Avatar192@!";
+   private String name = "ineuron";
+
+   
+   public String getId()
+   {
+     return id;
+   }
+   
+   public String getPin()
+   {
+     return pin;
+   }
+   
+   public String getName()
+   {
+     return name;
+   }
+   
+  
+   
+}
